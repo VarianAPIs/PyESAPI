652b1667497f1facfe56b60b0fdb76674228e08e
Michael Folkerts <michael.folekrts@varian.com>
2017-11-27 09:48:08 -0600

    added voxel volume helper function.

--- pysapi/tools/structures/__init__.py
+++ pysapi/tools/structures/__init__.py
@@ -1,3 +1,18 @@
 from .dvh import *
 from .ovh import *
 from .dth import *
+
+def voxel_volume(v_pts):
+    """Returns the volume of voxel elements in mm^3
+    
+    Args:
+        v_pts: a 4D numpy array of uniform grid voxel location points (x,y,z) for each voxel
+
+    Returns:
+        vox_vol: volume of the voxel @ (0,0,0)
+    """
+    vx = v_pts[1,0,0,0]-v_pts[0,0,0,0]
+    vy = v_pts[0,1,0,1]-v_pts[0,0,0,1]
+    vz = v_pts[0,0,1,2]-v_pts[0,0,0,2]
+
+    return vx*vy*vz
