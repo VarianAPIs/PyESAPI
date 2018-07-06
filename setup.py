1814009ca01e54a6643cec1df831ef7d402334e2
Michael Folkerts <michael.folkerts@varian.com>
2018-07-06 15:29:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name='pyesapi',
     version='0.1dev',
-    description='Python interface to Eclipse Scripting API"
-    author='Michael Folkerts, Varian Medical Systems'
-    author_email='Michael.Folkerts@varian.com'
+    description='Python interface to Eclipse Scripting API',
+    author='Michael Folkerts, Varian Medical Systems',
+    author_email='Michael.Folkerts@varian.com',
     license='MIT',
     packages=['pyesapi'],
     install_requires=[
