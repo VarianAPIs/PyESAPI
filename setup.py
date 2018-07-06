17e59aa31891425236f7daf9e6997f5a85866ffd
Michael Folkerts <michael.folkerts@varian.com>
2018-07-06 12:00:49 -0500

    Create setup.py
    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pyesapi',
+    version='0.1dev',
+    packages=['pyesapi',],
+    license=open('LICENSE').read(),
+    long_description='https://github.com/VarianAPIs/PyESAPI',
+)
