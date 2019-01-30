49ca14da38ae1c0aab75bc1e813d07b6530692d3
Michael Folkerts <Michael.Folkerts@varian.com>
2019-01-30 05:10:19 -0600

    testing setup.py install_requires

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from distutils.core import setup
 
 setup(
     name='pyesapi',
-    version='0.2dev',
+    version='0.3dev',
     description='Python interface to Eclipse Scripting API',
     author='Michael Folkerts, Varian Medical Systems',
     author_email='Michael.Folkerts@varian.com',
     license='MIT',
     packages=['pyesapi'],
+    install_requires=[
+        'pip @ git+https://github.com/pythonnet/pythonnet@d3ca2e8f9d221853f5c75fbb0ff4c7c85dafa70d#egg=pythonnet',
+    ],
 )
