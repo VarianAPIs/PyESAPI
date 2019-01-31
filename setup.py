ede446894abee5e5b21bfbb7f558eae01de79b3a
Michael Folkerts <Michael.Folkerts@varian.com>
2019-01-30 23:40:57 -0600

    added proper requirements

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(
     license='MIT',
     packages=['pyesapi'],
     install_requires=[
+        'pypiwin32',
+        'numpy',
+        'scipy'
         'pythonnet @ https://api.github.com/repos/pythonnet/pythonnet/tarball/d3ca2e8f9d221853f5c75fbb0ff4c7c85dafa70d', # some commit we know works
     ],
 )
