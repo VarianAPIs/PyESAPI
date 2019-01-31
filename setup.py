68089ae9249c733fb60436750f5f1beb7a323f2f
Michael Folkerts <Michael.Folkerts@varian.com>
2019-01-30 23:38:09 -0600

    dev version update before master merge

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyesapi',
-    version='0.4dev',
+    version='0.2dev',
     description='Python interface to Eclipse Scripting API',
     author='Michael Folkerts, Varian Medical Systems',
     author_email='Michael.Folkerts@varian.com',
