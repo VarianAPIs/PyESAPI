7b290e552fccc70b5a0b356fcf899e2210b56136
Michael Folkerts <michael.folkerts@varian.com>
2019-02-26 17:29:20 -0600

    Update setup.py
    cleaning up requirements

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
         'pypiwin32',
         'numpy',
         'scipy',
-        # 'pythonnet @ https://api.github.com/repos/pythonnet/pythonnet/tarball/d3ca2e8f9d221853f5c75fbb0ff4c7c85dafa70d', # some commit we know works
+        'pythonnet'
     ],
 )
