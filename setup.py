a41f337097fb8a076f3f9ae9710ff5c8f8fbef29
Michael Folkerts <michael.folkerts@varian.com>
2024-07-19 20:02:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,18 @@ from setuptools import find_packages
 setup(
     name='pyesapi',
     version='0.2.3',
-    description='Python interface to Eclipse Scripting API',
-    author='Michael Folkerts, Varian Medical Systems',
+    description='A customized Python interface to Eclipse Scripting API',
+    long_description='Leverages the pythonnet project to interface with dotnet CLI in real time. Helper functions and classes have been added to return numpy arrays and manage collections (of IEnumerable).'
+    author='Michael M. Folkerts, PhD at Varian, a Siemens Healthineers Company',
+    url='https://github.com/VarianAPIs/PyESAPI',
     author_email='Michael.Folkerts@varian.com',
     license='MIT',
     packages=find_packages(),
+    python_requires=">=3.7, <4", # see up to date active releases here: https://www.python.org/downloads/ 
     install_requires=[
         'numpy',
         'scipy',
-        'pythonnet', #  v2.3.0 tested to work with python 3.6
+        'pythonnet',
         'pynetdicom'
     ],
 )
